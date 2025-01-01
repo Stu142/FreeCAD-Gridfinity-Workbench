b45644e6b465f94a0fe4e5312026bc4a4a1b2b5c
Ruud Swinkels <ruudswinkels@hotmail.nl>
2025-01-01 14:53:22 +0100

    format all with ruff

--- freecad/gridfinity_workbench/commands.py
+++ freecad/gridfinity_workbench/commands.py
@@ -1,7 +1,17 @@
 import os
 import FreeCAD
 import FreeCADGui as Gui
-from .features import BinBlank, BinBase, Baseplate, SimpleStorageBin, MagnetBaseplate, ScrewTogetherBaseplate, EcoBin, PartsBin
+from .features import (
+    BinBlank,
+    BinBase,
+    Baseplate,
+    SimpleStorageBin,
+    MagnetBaseplate,
+    ScrewTogetherBaseplate,
+    EcoBin,
+    PartsBin,
+)
+
 
 class ViewProviderGridfinity(object):
     def __init__(self, obj, icon_fn=None):
@@ -9,12 +19,20 @@ class ViewProviderGridfinity(object):
         obj.Proxy = self
         self._check_attr()
         dirname = os.path.dirname(__file__)
-        self.icon_fn = icon_fn or os.path.join(dirname, "icons", "gridfinity_workbench_icon.svg")
+        self.icon_fn = icon_fn or os.path.join(
+            dirname, "icons", "gridfinity_workbench_icon.svg"
+        )
 
     def _check_attr(self):
-        ''' Check for missing attributes. '''
+        """Check for missing attributes."""
         if not hasattr(self, "icon_fn"):
-            setattr(self, "icon_fn", os.path.join(os.path.dirname(__file__), "icons", "gridfinity_workbench_icon.svg"))
+            setattr(
+                self,
+                "icon_fn",
+                os.path.join(
+                    os.path.dirname(__file__), "icons", "gridfinity_workbench_icon.svg"
+                ),
+            )
 
     def attach(self, vobj):
         self.vobj = vobj
@@ -30,6 +48,7 @@ class ViewProviderGridfinity(object):
     def __setstate__(self, state):
         if state and "icon_fn" in state:
             self.icon_fn = state["icon_fn"]
+
 
 class BaseCommand(object):
     NAME = ""
@@ -47,7 +66,11 @@ class BaseCommand(object):
 
     def Activated(self):
         Gui.doCommand("import freecad.gridfinity_workbench.commands")
-        Gui.doCommandGui("freecad.gridfinity_workbench.commands.{}.create()".format(self.__class__.__name__))
+        Gui.doCommandGui(
+            "freecad.gridfinity_workbench.commands.{}.create()".format(
+                self.__class__.__name__
+            )
+        )
         FreeCAD.ActiveDocument.recompute()
         Gui.SendMsgToActiveView("ViewFit")
 
@@ -59,7 +82,9 @@ class BaseCommand(object):
             part = Gui.ActiveDocument.ActiveView.getActiveObject("part")
 
             if body:
-                obj = FreeCAD.ActiveDocument.addObject("PartDesign::FeaturePython", cls.NAME)
+                obj = FreeCAD.ActiveDocument.addObject(
+                    "PartDesign::FeaturePython", cls.NAME
+                )
             else:
                 obj = FreeCAD.ActiveDocument.addObject("Part::FeaturePython", cls.NAME)
             ViewProviderGridfinity(obj.ViewObject, cls.Pixmap)
@@ -75,65 +100,72 @@ class BaseCommand(object):
         return obj
 
     def GetResources(self):
-        return {'Pixmap': self.Pixmap,
-                'MenuText': self.MenuText,
-                'ToolTip': self.ToolTip}
+        return {
+            "Pixmap": self.Pixmap,
+            "MenuText": self.MenuText,
+            "ToolTip": self.ToolTip,
+        }
 
 
 class CreateBinBlank(BaseCommand):
     NAME = "BinBlank"
     GRIDFINITY_FUNCTION = BinBlank
-    Pixmap = os.path.join(BaseCommand.ICONDIR, 'BinBlank.svg')
-    MenuText = 'Gridfinity Blank Bin'
-    ToolTip = 'Create a Gridfinity Blank Bin'
+    Pixmap = os.path.join(BaseCommand.ICONDIR, "BinBlank.svg")
+    MenuText = "Gridfinity Blank Bin"
+    ToolTip = "Create a Gridfinity Blank Bin"
+
 
 class CreateBinBase(BaseCommand):
     NAME = "BinBase"
     GRIDFINITY_FUNCTION = BinBase
-    Pixmap = os.path.join(BaseCommand.ICONDIR, 'BinBase.svg')
-    MenuText = 'Gridfinity Bin Base'
-    ToolTip = 'Create a Gridfinity Bin Base'
+    Pixmap = os.path.join(BaseCommand.ICONDIR, "BinBase.svg")
+    MenuText = "Gridfinity Bin Base"
+    ToolTip = "Create a Gridfinity Bin Base"
+
 
 class CreateSimpleStorageBin(BaseCommand):
     NAME = "SimpleStorageBin"
     GRIDFINITY_FUNCTION = SimpleStorageBin
-    Pixmap = os.path.join(BaseCommand.ICONDIR, 'SimpleStorageBin.svg')
-    MenuText = 'Gridfinity Simple Storage Bin'
-    ToolTip = 'Create a Gridfinity Simple Storage Bin'
+    Pixmap = os.path.join(BaseCommand.ICONDIR, "SimpleStorageBin.svg")
+    MenuText = "Gridfinity Simple Storage Bin"
+    ToolTip = "Create a Gridfinity Simple Storage Bin"
+
 
 class CreateEcoBin(BaseCommand):
     NAME = "EcoBin"
     GRIDFINITY_FUNCTION = EcoBin
-    Pixmap = os.path.join(BaseCommand.ICONDIR, 'eco_bin.svg')
-    MenuText = 'Gridfinity Eco Bin'
-    ToolTip = 'Create a Gridfinity Eco Bin'
+    Pixmap = os.path.join(BaseCommand.ICONDIR, "eco_bin.svg")
+    MenuText = "Gridfinity Eco Bin"
+    ToolTip = "Create a Gridfinity Eco Bin"
+
 
 class CreatePartsBin(BaseCommand):
     NAME = "PartsBin"
     GRIDFINITY_FUNCTION = PartsBin
-    Pixmap = os.path.join(BaseCommand.ICONDIR, 'parts_bin.svg')
-    MenuText = 'Gridfinity Parts Bin'
-    ToolTip = 'Create a Gridfinity Parts Bin'
+    Pixmap = os.path.join(BaseCommand.ICONDIR, "parts_bin.svg")
+    MenuText = "Gridfinity Parts Bin"
+    ToolTip = "Create a Gridfinity Parts Bin"
+
 
 class CreateBaseplate(BaseCommand):
     NAME = "Baseplate"
     GRIDFINITY_FUNCTION = Baseplate
-    Pixmap = os.path.join(BaseCommand.ICONDIR, 'Baseplate.svg')
-    MenuText = 'Gridfinity simple baseplate'
-    ToolTip = 'Create a Gridfinity Baseplate'
+    Pixmap = os.path.join(BaseCommand.ICONDIR, "Baseplate.svg")
+    MenuText = "Gridfinity simple baseplate"
+    ToolTip = "Create a Gridfinity Baseplate"
+
 
 class CreateMagnetBaseplate(BaseCommand):
     NAME = "MagnetBaseplate"
     GRIDFINITY_FUNCTION = MagnetBaseplate
-    Pixmap = os.path.join(BaseCommand.ICONDIR, 'magnet_baseplate.svg')
-    MenuText = 'Gridfinity magnet baseplate'
-    ToolTip = 'Create a Gridfinity Baseplate'
+    Pixmap = os.path.join(BaseCommand.ICONDIR, "magnet_baseplate.svg")
+    MenuText = "Gridfinity magnet baseplate"
+    ToolTip = "Create a Gridfinity Baseplate"
+
 
 class CreateScrewTogetherBaseplate(BaseCommand):
     NAME = "ScrewTogetherBaseplate"
     GRIDFINITY_FUNCTION = ScrewTogetherBaseplate
-    Pixmap = os.path.join(BaseCommand.ICONDIR, 'screw_together_baseplate.svg')
-    MenuText = 'Gridfinity Screw together baseplate'
-    ToolTip = 'Create a Gridfinity Screw Together Baseplate'
-
-
+    Pixmap = os.path.join(BaseCommand.ICONDIR, "screw_together_baseplate.svg")
+    MenuText = "Gridfinity Screw together baseplate"
+    ToolTip = "Create a Gridfinity Screw Together Baseplate"
--- freecad/gridfinity_workbench/init_gui.py
+++ freecad/gridfinity_workbench/init_gui.py
@@ -7,14 +7,18 @@ try:
     from FreeCADGui import Workbench
 except ImportError:
     App.Console.PrintWarning(
-        "you are using the GridfinityWorkbench with an old version of FreeCAD (<0.16)")
+        "you are using the GridfinityWorkbench with an old version of FreeCAD (<0.16)"
+    )
     App.Console.PrintWarning(
-        "the class Workbench is loaded, although not imported: magic")
+        "the class Workbench is loaded, although not imported: magic"
+    )
+
 
 class GridfinityWorkbench(Workbench):
     """
     class which gets initiated at starup of the gui
     """
+
     MenuText = "Gridfinity"
     ToolTip = "FreeCAD Gridfinity Workbench"
     Icon = os.path.join(ICONPATH, "gridfinity_workbench_icon.svg")
@@ -26,7 +30,8 @@ class GridfinityWorkbench(Workbench):
         "CreatePartsBin",
         "CreateBaseplate",
         "CreateMagnetBaseplate",
-        "CreateScrewTogetherBaseplate"]
+        "CreateScrewTogetherBaseplate",
+    ]
 
     def GetClassName(self):
         return "Gui::PythonWorkbench"
@@ -50,15 +55,14 @@ class GridfinityWorkbench(Workbench):
         self.appendToolbar("Gridfinity", self.toolbox)
         self.appendMenu("Gridfinity", self.toolbox)
 
-        Gui.addCommand('CreateBinBlank', CreateBinBlank())
-        Gui.addCommand('CreateBinBase', CreateBinBase())
-        Gui.addCommand('CreateSimpleStorageBin', CreateSimpleStorageBin())
-        Gui.addCommand('CreateEcoBin', CreateEcoBin())
-        Gui.addCommand('CreatePartsBin', CreatePartsBin())
-        Gui.addCommand('CreateBaseplate', CreateBaseplate())
-        Gui.addCommand('CreateMagnetBaseplate', CreateMagnetBaseplate())
-        Gui.addCommand('CreateScrewTogetherBaseplate', CreateScrewTogetherBaseplate())
-
+        Gui.addCommand("CreateBinBlank", CreateBinBlank())
+        Gui.addCommand("CreateBinBase", CreateBinBase())
+        Gui.addCommand("CreateSimpleStorageBin", CreateSimpleStorageBin())
+        Gui.addCommand("CreateEcoBin", CreateEcoBin())
+        Gui.addCommand("CreatePartsBin", CreatePartsBin())
+        Gui.addCommand("CreateBaseplate", CreateBaseplate())
+        Gui.addCommand("CreateMagnetBaseplate", CreateMagnetBaseplate())
+        Gui.addCommand("CreateScrewTogetherBaseplate", CreateScrewTogetherBaseplate())
 
     def Activated(self):
         pass
@@ -68,4 +72,3 @@ class GridfinityWorkbench(Workbench):
 
 
 Gui.addWorkbench(GridfinityWorkbench())
-
