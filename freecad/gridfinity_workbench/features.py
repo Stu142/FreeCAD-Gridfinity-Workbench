e45f4cc0b80db6119a9e38e3a9c90ccce0d49ade
Stu142 <74122014+Stu142@users.noreply.github.com>
2025-03-08 13:18:39 -0400

    Update freecad/gridfinity_workbench/features.py
    Co-authored-by: Grzegorz Kwacz <gkwacz@hotmail.com>

--- freecad/gridfinity_workbench/features.py
+++ freecad/gridfinity_workbench/features.py
@@ -985,15 +985,7 @@ class CustomEcoBin(FoundationGridfinity):
         obj.Proxy = self
 
     def generate_gridfinity_shape(self, obj: fc.DocumentObject) -> Part.Shape:
-        """Generate BinBlanek Shape.
-
-        Args:
-            obj (FreeCAD.DocumentObject): Document object.
-
-        Returns:
-            Part.Shape: Bin Blank shape
-
-        """
+        """Generate EcoBin Shape."""
         ## calculated here
         if obj.NonStandardHeight:
             obj.TotalHeight = obj.CustomHeight
